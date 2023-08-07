e32a5b33507faa875a17c609fe1097f66026f927
mehargee <mehrgee826@gmail.com>
2023-08-07 22:05:34 +0500

    coinbase-web-start

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+
+const Navbar = () => {
+    return (
+      <>
+      <div className="a"> 
+      <div className="b"> <img src="/images/Logo.png" alt="logo" />
+      </div>
+      </div>
+      </>
+    )
+  }
+  
+  export default Navbar;
+  
